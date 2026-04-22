f71f85edb8faec8704abdfb4ecb4c7a816d996d2
danielgarciavico.alum <danielgarciavico.alum@insestatut.cat>
2026-04-22 19:47:18 +0200

    electroon instalado

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("HEY")
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    // Creem una nova finestra especificant les seves dimensions
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    // Carreguem l'arxiu inicial de la nostra aplicació web
+    win.loadFile('index.html');
+}
+// Quan electron hagi carregat les seves dependencies mostra la finestra
+app.whenReady().then(() => {
+    createWindow()
+});
