c3fc00aacca83a1190d4ecc1820d4824ab3b14fc
danielgarciavico.alum <danielgarciavico.alum@insestatut.cat>
2026-04-23 16:06:19 +0200

    crear tablero

---
+++ README.md
@@ -0,0 +1,5 @@
+QUE NECESITAMOS
+
+un  index.html con la estructura basica para mostrar el tablero de manera cutre para avanzar en la programacion del juego
+
+classe Takuzu en la que gestionamos la  partida
--- clases/Takuzu.js
+++ clases/Takuzu.js
@@ -9,14 +9,43 @@ class Takuzu {
         this.#tablero = tableroElegido;
     };
 
-    mostrarTablero() {
-        for (let i = 0; i < this.#tablero.length; i++) {
-            console.log(this.#tablero[i]);
+    crearTablero() {
+        let f = 0;
+        let c = 0;
+
+        let tablero = document.createElement("div");
+        tablero.className = "tablero";
+
+        for (let x = 0; x < this.#tablero.length; x++) {
+            for (let y = 0; y < this.#tablero[x].length; y++) {
+                let casilla = document.createElement("div");
+                casilla.textContent = this.#tablero;
+                casilla.id = "" + f + c;
+                
+                tablero.append(casilla);
+                c++;
+            };
+            f++;
         };
     };
 
 
+
+
+
+
+
+
+
+
 };
+
+
+
+
+
+
+
 
 let c = new Takuzu([
     ["0", "", "", ""],
